cb1bc586dc967a4b81760ccd0f54dcca63b0fdb5
Nader Elziny <naderelziny805@gmail.com>
2023-04-02 16:00:54 +0300

    Updated the virtual environment from venv to pipenv.

--- django-app/carvice_backend/settings.py
+++ django-app/carvice_backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s%467a4-nc8t1vj)lp10z*)^x(o$h*m92c(_3i7o4mux&u-19t'
+SECRET_KEY = 'django-insecure-6=oz-u!&523r$_$bsrpfo74l1wk54g@**qokjox=+@4jsvngh9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
