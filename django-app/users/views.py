a2c3cd344108e2049816cd73ea76864d8802a00b
Nader Elziny <naderelziny805@gmail.com>
2023-04-04 23:15:04 +0300

    Globalized User variable as it is shared between more than one class in the file.

--- django-app/users/views.py
+++ django-app/users/views.py
@@ -1,3 +1,7 @@
+import datetime
+
+import jwt
+
 from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.views import APIView
@@ -5,6 +9,7 @@ from .serializers import UserSerializer
 from django.contrib.auth import get_user_model
 
 # Create your views here.
+User = get_user_model()
 
 
 class SignupView(APIView):
@@ -19,13 +24,13 @@ class SignupView(APIView):
 
 
 class SigninView(APIView):
-    User = get_user_model()
 
-    def post(self, request) -> Response:
+    @staticmethod
+    def post(request) -> Response:
         """Handles the login POST requests."""
         username = request.data.get("username")
         password = request.data.get("password")
-        user = self.User.objects.filter(username=username).first()
+        user = User.objects.filter(username=username).first()
         if not user:
             raise AuthenticationFailed("Username doesn't exist.")
 
