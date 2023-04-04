dd254dc13fa964fd7677ff9cda8891e7ee061e55
Nader Elziny <naderelziny805@gmail.com>
2023-04-04 18:36:19 +0300

    Hashed the passwords that are going to be stored in the database.

--- django-app/users/serializers.py
+++ django-app/users/serializers.py
@@ -6,3 +6,14 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'first_name', 'last_name', 'username', 'email', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data) -> User:
+        """Handles extra operations in user creation (e.g. Hashing passwords)."""
+        user_instance = self.Meta.model(**validated_data)
+        if password := validated_data.pop('password', None):
+            user_instance.set_password(password)
+        user_instance.save()
+        return user_instance
