c46e14fafdd158cf1bc14c16b9b806d91582b8bb
Nader Elziny <naderelziny805@gmail.com>
2023-04-04 19:16:03 +0300

    Added a new endpoint with its view [signin].

--- django-app/users/urls.py
+++ django-app/users/urls.py
@@ -2,9 +2,10 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from .views import SignupView
+from .views import SignupView, SigninView
 
 urlpatterns = [
     path('signup', SignupView.as_view()),
+    path('signin', SigninView.as_view()),
 
 ]
