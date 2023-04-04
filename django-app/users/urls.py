dfea66a4761c9630a48327c985daeaf8b305d19a
Nader Elziny <naderelziny805@gmail.com>
2023-04-04 23:28:27 +0300

    Added an endpoint to the sign out view.

--- django-app/users/urls.py
+++ django-app/users/urls.py
@@ -2,11 +2,12 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from .views import SignupView, SigninView, UserView
+from .views import SignupView, SigninView, UserView, SignOutView
 
 urlpatterns = [
     path('signup', SignupView.as_view()),
     path('signin', SigninView.as_view()),
+    path('signout', SignOutView.as_view()),
     path('user', UserView.as_view()),
 
 ]
