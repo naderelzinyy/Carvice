adb7c9a07a50c05e74aad561228ef3f650bb9ac9
Nader Elziny <naderelziny805@gmail.com>
2023-04-04 19:16:51 +0300

    Added SigninView class to handle the view of sign in process.

--- django-app/users/views.py
+++ django-app/users/views.py
@@ -1,6 +1,9 @@
 from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.views import APIView
 from .serializers import UserSerializer
+from django.contrib.auth import get_user_model
+
 # Create your views here.
 
 
@@ -13,3 +16,22 @@ class SignupView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+
+
+class SigninView(APIView):
+    User = get_user_model()
+
+    def post(self, request) -> Response:
+        """Handles the login POST requests."""
+        username = request.data.get("username")
+        password = request.data.get("password")
+        user = self.User.objects.filter(username=username).first()
+        if not user:
+            raise AuthenticationFailed("Username doesn't exist.")
+
+        if not user.check_password(password):
+            raise AuthenticationFailed("Password isn't correct.")
+
+        return Response({
+            "message": "success"
+        })
