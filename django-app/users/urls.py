edee8a88283fedc49e1fdb10fe15d426a03c41f8
Nader Elziny <naderelziny805@gmail.com>
2023-04-04 23:15:21 +0300

    Added an endpoint for the UserView.

--- django-app/users/urls.py
+++ django-app/users/urls.py
@@ -2,10 +2,11 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from .views import SignupView, SigninView
+from .views import SignupView, SigninView, UserView
 
 urlpatterns = [
     path('signup', SignupView.as_view()),
     path('signin', SigninView.as_view()),
+    path('user', UserView.as_view()),
 
 ]
