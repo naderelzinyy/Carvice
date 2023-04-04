134b7177ea43cbf92c2dbad86ffedec7fd48a711
Nader Elziny <naderelziny805@gmail.com>
2023-04-04 04:39:41 +0300

    Created a new app [users] to handle the authentication process of users.

---
+++ django-app/users/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# Create your models here.
+
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    username = models.CharField(max_length=255, unique=True)
+    email = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255, unique=True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
