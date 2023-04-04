69392d5381bde9dd7d012eac1ec79b892bcbeaa5
Nader Elziny <naderelziny805@gmail.com>
2023-04-04 05:17:36 +0300

    Created an API based registration view and serializer.

--- django-app/users/views.py
+++ django-app/users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import UserSerializer
 # Create your views here.
+
+
+class SignupView(APIView):
+
+    @staticmethod
+    def post(request) -> Response:
+        """Handles sending POST requests."""
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
