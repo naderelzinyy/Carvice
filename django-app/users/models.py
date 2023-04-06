74701ed12a7c33f0f8cef3c5bc34061ff6d98adf
Nader Elziny <naderelziny805@gmail.com>
2023-04-06 03:55:54 +0300

    Made a new migration after solving the unique password issue.

--- django-app/users/models.py
+++ django-app/users/models.py
@@ -10,7 +10,7 @@ class User(AbstractUser):
     last_name = models.CharField(max_length=255)
     username = models.CharField(max_length=255, unique=True)
     email = models.CharField(max_length=255, unique=True)
-    password = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
 
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = []
