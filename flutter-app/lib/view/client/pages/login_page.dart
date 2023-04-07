7723e7bdb948f0c2a3e358861dc23fa8cd1d4418
Nader Elziny <naderelziny805@gmail.com>
2023-04-07 04:23:37 +0300

    Added the api request on sign in button event.

--- flutter-app/lib/view/client/pages/login_page.dart
+++ flutter-app/lib/view/client/pages/login_page.dart
@@ -1,105 +1,104 @@
+import 'package:carvice_frontend/services/authentication.dart';
+import 'package:carvice_frontend/view/client/pages/signup_page.dart';
+import 'package:carvice_frontend/widgets/button.dart';
+import 'package:carvice_frontend/widgets/text_field.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:carvice_frontend/widgets/text_field.dart';
-import 'package:carvice_frontend/widgets/button.dart';
-import 'package:carvice_frontend/view/client/pages/signup_page.dart';
-
 
 class LoginPage extends StatelessWidget {
   LoginPage({Key? key}) : super(key: key);
   final TextEditingController usernameController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
-
-
+  var response = false;
+  String text = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
         child: SafeArea(
             child: Container(
-              width: double.infinity,
-                padding: const EdgeInsets.all(15.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+          width: double.infinity,
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 10),
+              Container(
+                alignment: Alignment.center,
+                child: Image.asset(
+                  'assets/images/transparent_logo.png',
+                  width: MediaQuery.of(context).size.width * 0.6,
+                  height: MediaQuery.of(context).size.width * 0.6,
+                  fit: BoxFit.contain,
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Please Login To Your Account',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+              const SizedBox(height: 15),
+              CustomTextFiled(
+                  controller: usernameController,
+                  hintText: "username",
+                  textInputType: TextInputType.name,
+                  obscureText: false),
+              const SizedBox(height: 10),
+              CustomTextFiled(
+                  controller: passwordController,
+                  hintText: "password",
+                  textInputType: TextInputType.text,
+                  obscureText: true),
+              const SizedBox(height: 20),
+              CustomButton(
+                  btnText: "Sign In",
+                  onTap: () async {
+                    Future<bool> response = Authenticator().authenticate({
+                      "username": usernameController.text,
+                      "password": passwordController.text
+                    });
+                    text = await response ? "Logged in" : "";
+                    print(text);
+                    // Add your custom logic here
+                  }),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  const SizedBox(height: 10),
-                  Container(
-                    alignment: Alignment.center,
-                     child: Image.asset(
-                       'assets/images/transparent_logo.png',
-                       width: MediaQuery.of(context).size.width * 0.6,
-                       height: MediaQuery.of(context).size.width * 0.6,
-                       fit: BoxFit.contain,
-                     ),
-                  ),
-                    const SizedBox(height: 20),
-                    const Text(
-                    'Please Login To Your Account',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 18,
-                      fontWeight: FontWeight.normal,
-                    ),
-                  ),
-                  const SizedBox(height: 15),
-                   CustomTextFiled(controller: usernameController,
-                     hintText: "username", textInputType: TextInputType.name,
-                       obscureText: false),
-                  const SizedBox(height: 10),
-                  CustomTextFiled(controller: passwordController,
-                      hintText: "password", textInputType: TextInputType.text,
-                      obscureText: true),
-                  const SizedBox(height: 20),
-                   CustomButton(btnText: "Sign In",
+                  const Text("Forget your password?"),
+                  InkWell(
                     onTap: () {
-                      print('Login button tapped');
-                      // Add your custom logic here
-                    }),
-                  const SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children:  [
-                      const Text(
-                          "Forget your password?"
-                      ),
-                      InkWell(
-                        onTap: (){
-                          print("reset");
-                        },
-                        child: const Text(
-                            " Reset Password"
-                        ),
-                      )
-                    ],
-                  ),
+                      print("reset");
+                    },
+                    child: const Text(" Reset Password"),
+                  )
                 ],
               ),
-            )
-        ),
+            ],
+          ),
+        )),
       ),
       bottomNavigationBar: Container(
-        height: 100,
-        alignment: Alignment.center,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-
+          height: 100,
+          alignment: Alignment.center,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text(
-                  "Don't have an account?"
+              const Text("Don't have an account?"),
+              InkWell(
+                onTap: () {
+                  Get.to(() => SignUp(),
+                      transition: Transition.fade,
+                      duration: const Duration(seconds: 1));
+                },
+                child: const Text(" Sign Up"),
               ),
-              InkWell( onTap: (){
-                Get.to(() => SignUp(), transition: Transition.fade,
-                    duration: const Duration(seconds: 1));
-              },
-
-                child: const Text(
-                    " Sign Up"
-
-                ),
-              )
             ],
-          )
-      ),
+          )),
     );
   }
 }
