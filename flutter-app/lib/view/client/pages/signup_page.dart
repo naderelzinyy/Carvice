eda97fe6ea15a5716ce7221d4a4f923e309f5777
Nader Elziny <naderelziny805@gmail.com>
2023-04-08 16:08:26 +0300

    Called Authenticator.register to send user data to the api.

--- flutter-app/lib/view/client/pages/signup_page.dart
+++ flutter-app/lib/view/client/pages/signup_page.dart
@@ -22,60 +22,86 @@ class SignUp extends StatelessWidget {
       body: SingleChildScrollView(
         child: SafeArea(
             child: Container(
-              width: double.infinity,
-              padding: const EdgeInsets.all(15.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    alignment: Alignment.center,
-                    child: Image.asset(
-                      'assets/images/transparent_logo.png',
-                      width: MediaQuery.of(context).size.width * 0.45,
-                      height: MediaQuery.of(context).size.width * 0.45,
-                      fit: BoxFit.contain,
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  const Text(
-                    'Create New Account',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 18,
-                      fontWeight: FontWeight.normal,
-                    ),
-                  ),
-                  const SizedBox(height: 15),
-                  CustomTextFiled(controller: fullNameController,
-                      hintText: "full name", textInputType: TextInputType.name,
-                      obscureText: false),
-                  const SizedBox(height: 10),
-                  CustomTextFiled(controller: usernameController,
-                      hintText: "username", textInputType: TextInputType.name,
-                      obscureText: false),
-                  const SizedBox(height: 10),
-                  CustomTextFiled(controller: emailController,
-                      hintText: "email", textInputType: TextInputType.emailAddress,
-                      obscureText: false),
-                  const SizedBox(height: 10),
-                  CustomTextFiled(controller: phoneController,
-                      hintText: "phone number", textInputType: TextInputType.phone,
-                      obscureText: false),
-                  const SizedBox(height: 10),
-                  CustomTextFiled(controller: passwordController,
-                      hintText: "password", textInputType: TextInputType.text,
-                      obscureText: true),
-                  const SizedBox(height: 10),
-                  CustomTextFiled(controller: confPasswordController,
-                      hintText: "password conformation", textInputType: TextInputType.text,
-                      obscureText: true),
-                  const SizedBox(height: 25),
-                    CustomButton(btnText: "Sign Up",
-                       onTap: () {
-            print('sign up button tapped');
-            // Add your custom logic here
-            },),
-                ],
+          width: double.infinity,
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                alignment: Alignment.center,
+                child: Image.asset(
+                  'assets/images/transparent_logo.png',
+                  width: MediaQuery.of(context).size.width * 0.45,
+                  height: MediaQuery.of(context).size.width * 0.45,
+                  fit: BoxFit.contain,
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Create New Account',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+              const SizedBox(height: 15),
+              CustomTextFiled(
+                  controller: firstNameController,
+                  hintText: "first name",
+                  textInputType: TextInputType.name,
+                  obscureText: false),
+              const SizedBox(height: 10),
+              CustomTextFiled(
+                  controller: lastNameController,
+                  hintText: "last name",
+                  textInputType: TextInputType.name,
+                  obscureText: false),
+              const SizedBox(height: 10),
+              CustomTextFiled(
+                  controller: usernameController,
+                  hintText: "username",
+                  textInputType: TextInputType.name,
+                  obscureText: false),
+              const SizedBox(height: 10),
+              CustomTextFiled(
+                  controller: emailController,
+                  hintText: "email",
+                  textInputType: TextInputType.emailAddress,
+                  obscureText: false),
+              const SizedBox(height: 10),
+              CustomTextFiled(
+                  controller: phoneController,
+                  hintText: "phone number",
+                  textInputType: TextInputType.phone,
+                  obscureText: false),
+              const SizedBox(height: 10),
+              CustomTextFiled(
+                  controller: passwordController,
+                  hintText: "password",
+                  textInputType: TextInputType.text,
+                  obscureText: true),
+              const SizedBox(height: 10),
+              CustomTextFiled(
+                  controller: confPasswordController,
+                  hintText: "password conformation",
+                  textInputType: TextInputType.text,
+                  obscureText: true),
+              const SizedBox(height: 25),
+              CustomButton(
+                //todo: Disable the sign up button by default and enable it once the passwordController.text is equal to confPasswordController.text
+                btnText: "Sign Up",
+                onTap: () {
+                  print('sign up button tapped');
+                  Future<bool> is_signed_up = Authenticator().register({
+                    "first_name": firstNameController.text,
+                    "last_name": lastNameController.text,
+                    "username": usernameController.text,
+                    "email": emailController.text,
+                    "password": passwordController.text,
+                  });
+                  // Add your custom logic here
+                },
               ),
             )
         ),
