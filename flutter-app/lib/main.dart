cc5cb9a3b83329fd81bdeb129aa537cd7146c449
Hasan Alani <59187679+hasanalaniIT@users.noreply.github.com>
2023-04-10 22:40:11 +0300

    Adding firebase initializeApp at main to be started at app run time

--- flutter-app/lib/main.dart
+++ flutter-app/lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:carvice_frontend/view/chat/chat_list_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:carvice_frontend/view/start/pages/splash_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 class MyApp extends StatelessWidget {
