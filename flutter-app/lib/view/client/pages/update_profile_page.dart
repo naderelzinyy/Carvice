061d89a22c01718ed0603b75216ea15858189aef
Nader Elziny <naderelziny805@gmail.com>
2023-05-01 23:49:37 +0300

    Showed DB records in ProfileForm.

--- flutter-app/lib/view/client/pages/update_profile_page.dart
+++ flutter-app/lib/view/client/pages/update_profile_page.dart
@@ -1,28 +1,26 @@
 import 'package:flutter/material.dart';
 
+import '../../../services/authentication.dart';
 import '../../../widgets/app_navigation.dart';
 import '../../../widgets/profile_form.dart';
-
-
 
 class ClientUpdateProfilePage extends StatelessWidget {
   const ClientUpdateProfilePage({Key? key}) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: const AppNavigation(
         title: "Update Profile",
         automaticallyCallBack: true,
       ),
-      body:ProfileForm(
-        image:    'assets/images/profile.png',
-        firstName: 'Spong',
-        lastName: 'Bob',
-        userName: 'Spongbob',
-        email: 'Spongbob@carvice.com',
-        phoneNumber: '1234567890',
+      body: ProfileForm(
+        image: 'assets/images/profile.png',
+        firstName: token!["first_name"],
+        lastName: token!["last_name"],
+        userName: token!["username"],
+        email: token!["email"],
+        phoneNumber: token!["phone_number"],
         onTap: () {
           // TODO: Add save functionality here
           print("save updates btn pressed in client");
