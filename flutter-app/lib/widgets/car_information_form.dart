b2b4476232d41677ae89e63d165735f21eb9bfe3
Nader Elziny <naderelziny805@gmail.com>
2023-06-03 03:52:13 +0300

    Called addCar function in order to make an api call.

--- flutter-app/lib/widgets/car_information_form.dart
+++ flutter-app/lib/widgets/car_information_form.dart
@@ -63,131 +63,134 @@ class _CarsFormState extends State<CarsForm> {
   @override
   Widget build(BuildContext context) {
     final btnText = widget.update ? 'saveChanges'.tr : 'save'.tr;
-     return Scaffold(
-        body: SingleChildScrollView(
-          child: Container(
-                padding: const EdgeInsets.only(
-                top: 80,
-                left: 35,
-                right: 35
-            ),
-            child: Column(
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.only(top: 80, left: 35, right: 35),
+          child: Column(
+            children: [
+              SizedBox(
+                width: 110,
+                height: 110,
+                child: Stack(
                   children: [
-                      SizedBox(
-                        width: 110,
-                        height: 110,
-                        child: Stack(
-                          children: [
-                                  SizedBox(
-                                  width: 100,
-                                  height: 100,
-                                      child: ClipRRect(
-                                          borderRadius: BorderRadius.circular(100),
-                                        child: Image.asset(
-                                              'assets/images/profile.png',
-                                            fit: BoxFit.contain,
-                                        ),
-                                  ),
-                                  ),
-                          Positioned(
-                            bottom: 0,
-                            right: 0,
-                            child: SizedBox(
-                            width: 40,
-                            height: 40,
-                                      child: Container(
-                                        decoration: BoxDecoration(
-                                        shape: BoxShape.circle,
-                                        color: MainColors.mainColor,
+                    SizedBox(
+                      width: 100,
+                      height: 100,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(100),
+                        child: Image.asset(
+                          'assets/images/profile.png',
+                          fit: BoxFit.contain,
+                        ),
                       ),
-                                        child: IconButton(
-                                          onPressed:  () {
-                                              // TODO: Add save functionality here
-                                              print("save updates btn pressed in client");
-                                          },
-                                        icon: const Icon(Icons.edit),
-                                        color: Colors.white,
-                                        highlightColor: Colors.transparent,
-                                        splashColor: Colors.transparent,
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: SizedBox(
+                        width: 40,
+                        height: 40,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: MainColors.mainColor,
+                          ),
+                          child: IconButton(
+                            onPressed: () {
+                              // TODO: Add save functionality here
+                              print("save updates btn pressed in client");
+                            },
+                            icon: const Icon(Icons.edit),
+                            color: Colors.white,
+                            highlightColor: Colors.transparent,
+                            splashColor: Colors.transparent,
+                          ),
+                        ),
                       ),
-                      ),
-                      ),
-                      ),
-                      ],
-                      ),
-                      ),
-            const SizedBox(height: 30),
-            CustomTextFiled(
-              controller: _carBrandController,
-              hintText: 'enterCarBrand'.tr,
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 30),
+              CustomTextFiled(
+                  controller: _carBrandController,
+                  hintText: 'enterCarBrand'.tr,
+                  textInputType: TextInputType.text,
+                  obscureText: false),
+              const SizedBox(height: 16.0),
+              CustomTextFiled(
+                  controller: _carSeriesController,
+                  hintText: 'enterCarSeries'.tr,
+                  textInputType: TextInputType.text,
+                  obscureText: false),
+              const SizedBox(height: 16.0),
+              CustomTextFiled(
+                controller: _carModelController,
                 textInputType: TextInputType.text,
-                obscureText: false
-            ),
-            const SizedBox(height: 16.0),
-            CustomTextFiled(
-              controller: _carSeriesController,
-              hintText: 'enterCarSeries'.tr,
+                obscureText: false,
+                hintText: 'enterCarModel'.tr,
+              ),
+              const SizedBox(height: 16.0),
+              CustomTextFiled(
+                controller: _carYearController,
+                textInputType: TextInputType.number,
+                obscureText: false,
+                hintText: 'enterCarYear'.tr,
+              ),
+              const SizedBox(height: 16.0),
+              CustomTextFiled(
+                controller: _carFuelController,
                 textInputType: TextInputType.text,
-                obscureText: false
-            ),
-            const SizedBox(height: 16.0),
-            CustomTextFiled(
-              controller: _carModelController,
-              textInputType: TextInputType.text,
-              obscureText: false,
-              hintText: 'enterCarModel'.tr,
-            ),
-            const SizedBox(height: 16.0),
-            CustomTextFiled(
-              controller: _carYearController,
-              textInputType: TextInputType.number,
-              obscureText: false,
-              hintText: 'enterCarYear'.tr,
-            ),
-            const SizedBox(height: 16.0),
-            CustomTextFiled(
-              controller: _carFuelController,
-              textInputType: TextInputType.text,
-              obscureText: false,
-              hintText: 'enterCarFuel'.tr,
-            ),
-            const SizedBox(height: 16.0),
-            CustomTextFiled(
-              controller: _carGearController,
-              textInputType: TextInputType.text,
-              obscureText: false,
-              hintText: 'enterCarGear'.tr,
-            ),
-            const SizedBox(height: 16.0),
-            CustomTextFiled(
-              controller: _carEnginePowerController,
-              textInputType: TextInputType.number,
-              obscureText: false,
-              hintText: 'enterCarEnginePower'.tr,
-            ),
-            const SizedBox(height: 25.0),
-            CustomButton(
-              btnText: btnText,
-              onTap: _submitForm,
-            ),
-                    const SizedBox(height: 16.0),
-          ],
+                obscureText: false,
+                hintText: 'enterCarFuel'.tr,
+              ),
+              const SizedBox(height: 16.0),
+              CustomTextFiled(
+                controller: _carGearController,
+                textInputType: TextInputType.text,
+                obscureText: false,
+                hintText: 'enterCarGear'.tr,
+              ),
+              const SizedBox(height: 16.0),
+              CustomTextFiled(
+                controller: _carEnginePowerController,
+                textInputType: TextInputType.number,
+                obscureText: false,
+                hintText: 'enterCarEnginePower'.tr,
+              ),
+              const SizedBox(height: 25.0),
+              CustomButton(
+                btnText: btnText,
+                onTap: (() => {
+                      AccountManager().addCar({
+                        "owner": token!['id'].toString(),
+                        "brand": _carBrandController.text,
+                        "series": _carSeriesController.text,
+                        "model": _carModelController.text,
+                        "year": _carYearController.text,
+                        "gear": _carGearController.text,
+                        "fuel": _carFuelController.text,
+                        "engine_power": _carEnginePowerController.text,
+                      })
+                    }),
+              ),
+              const SizedBox(height: 16.0),
+            ],
+          ),
         ),
       ),
-    ),
-       bottomNavigationBar: Container(
-           height: 50,
-           alignment: Alignment.center,
-           child: const Row(
-             mainAxisAlignment: MainAxisAlignment.center,
-
-             children: [
-               Text(
-                 "Made with ♥ by Carvice team",
-               ),
-             ],
-           )
-       ),
-     );
+      bottomNavigationBar: Container(
+          height: 50,
+          alignment: Alignment.center,
+          child: const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Made with ♥ by Carvice team",
+              ),
+            ],
+          )),
+    );
   }
 }
