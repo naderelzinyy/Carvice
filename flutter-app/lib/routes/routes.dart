1a9896f97a7b6256330e1478bb816dcacbe9bfde
Maryam Alrubaye <maryamalrubaye@dizaynvip.com>
2023-06-07 09:11:42 +0300

    Added new created pages to the system router.

--- flutter-app/lib/routes/routes.dart
+++ flutter-app/lib/routes/routes.dart
@@ -1,6 +1,7 @@
 import 'package:carvice_frontend/view/client/pages/add_new_car_page.dart';
 import 'package:carvice_frontend/view/client/pages/carslist_page.dart';
 import 'package:carvice_frontend/view/client/pages/edit_car_info_page.dart';
+import 'package:carvice_frontend/view/client/pages/mechanic_profile_page.dart';
 import 'package:carvice_frontend/view/client/pages/update_profile_page.dart';
 import 'package:carvice_frontend/view/mechanic/pages/userprofile_page.dart';
 import 'package:get/get.dart';
@@ -12,6 +13,7 @@ import 'package:carvice_frontend/view/general/pages/splash_page.dart';
 
 import '../view/client/pages/chatlist_page.dart';
 import '../view/client/pages/home_page.dart';
+import '../view/client/pages/mechaniclist_page.dart';
 import '../view/client/pages/userprofile_page.dart';
 import '../view/general/pages/aboutus_page.dart';
 import '../view/mechanic/pages/home_page.dart';
@@ -37,6 +39,8 @@ class Routers {
   static String editCarPage = "/edit_car";
   static String addCarPage = "/add_car";
   static String aboutUsPage = "/about_us";
+  static String listOfMechanics = "/list_of_mechanics";
+  static String mechanicProfileInClient = "/mechanic_profile_in_client";
 
 
 
@@ -56,6 +60,8 @@ class Routers {
   static String getEditCarPageRoute(String carID) => '$editCarPage?carID=$carID';
   static String getAddCarPageRoute() => addCarPage;
   static String getAboutUsPageRoute() => aboutUsPage;
+  static String getListOfMechanicsPageRoute() => listOfMechanics;
+  static String getMechanicProfileInClientRoute() => mechanicProfileInClient;
 
 
   static List<GetPage> routes = [
@@ -160,6 +166,18 @@ class Routers {
     GetPage(
       name: aboutUsPage,
       page: () => const   AboutUsPage(),
+      transition: Transition.fade,
+      transitionDuration: const Duration(milliseconds: 20),
+    ),
+    GetPage(
+      name: listOfMechanics,
+      page: () => const   MechanicListPage(),
+      transition: Transition.fade,
+      transitionDuration: const Duration(milliseconds: 20),
+    ),
+    GetPage(
+      name: mechanicProfileInClient,
+      page: () => const MechanicProfile(),
       transition: Transition.fade,
       transitionDuration: const Duration(milliseconds: 20),
     ),
