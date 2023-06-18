525ca3dbbdb7223d46740bbb6eefc7470101be25
Hasan Alani <59187679+hasanalaniIT@users.noreply.github.com>
2023-06-18 20:25:33 +0300

    Added translation to turkish language to bank card information page

--- flutter-app/lib/view/general/pages/payment/bank_card_information.dart
+++ flutter-app/lib/view/general/pages/payment/bank_card_information.dart
@@ -40,121 +40,120 @@ class CardInformationState extends State<CardInformation> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppNavigation(
-        title: 'Payment'.tr,
-        automaticallyCallBack: true,
-      ),
-      resizeToAvoidBottomInset: false,
-      body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            CreditCardWidget(
-              cardNumber: cardNumber,
-              expiryDate: expiryDate,
-              cardHolderName: cardHolderName,
-              cvvCode: cvvCode,
-              bankName: 'Debit/Credit Card',
-              frontCardBorder: Border.all(color: Colors.black45),
-              backCardBorder: Border.all(color: Colors.black45),
-              showBackView: isCvvFocused,
-              obscureCardNumber: true,
-              obscureCardCvv: true,
-              isHolderNameVisible: true,
-              cardBgColor: MainColors.mainColor,
-              onCreditCardWidgetChange: (CreditCardBrand creditCardBrand) {},
-            ),
-            Expanded(
-              child: SingleChildScrollView(
-                child: Column(
-                  children: <Widget>[
-                    CreditCardForm(
-                      formKey: formKey,
-                      obscureCvv: true,
-                      obscureNumber: true,
-                      cardNumber: cardNumber,
-                      cvvCode: cvvCode,
-                      isHolderNameVisible: true,
-                      isCardNumberVisible: true,
-                      isExpiryDateVisible: true,
-                      cardHolderName: cardHolderName,
-                      expiryDate: expiryDate,
-                      themeColor: Colors.blue,
-                      textColor: Colors.black,
-                      cardNumberDecoration: InputDecoration(
-                        labelText: 'Number',
-                        hintText: 'XXXX XXXX XXXX XXXX',
-                        hintStyle: const TextStyle(color: Colors.grey),
-                        labelStyle: const TextStyle(color: Colors.grey),
-                        focusedBorder: border,
-                        enabledBorder: border,
-                      ),
-                      expiryDateDecoration: InputDecoration(
-                        hintStyle: const TextStyle(color: Colors.grey),
-                        labelStyle: const TextStyle(color: Colors.grey),
-                        focusedBorder: border,
-                        enabledBorder: border,
-                        labelText: 'Expired Date',
-                        hintText: 'XX/XX',
-                      ),
-                      cvvCodeDecoration: InputDecoration(
-                        hintStyle: const TextStyle(color: Colors.grey),
-                        labelStyle: const TextStyle(color: Colors.grey),
-                        focusedBorder: border,
-                        enabledBorder: border,
-                        labelText: 'CVV',
-                        hintText: 'XXX',
-                      ),
-                      cardHolderDecoration: InputDecoration(
-                        hintStyle: const TextStyle(color: Colors.grey),
-                        labelStyle: const TextStyle(color: Colors.grey),
-                        focusedBorder: border,
-                        enabledBorder: border,
-                        labelText: 'Card Holder',
-                      ),
-                      onCreditCardModelChange: onCreditCardModelChange,
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    GestureDetector(
-                      onTap: _onValidate,
-                      child: Container(
-                        margin: const EdgeInsets.symmetric(
-                            horizontal: 16, vertical: 8),
-                        decoration: BoxDecoration(
-                          gradient: const LinearGradient(
-                            colors: <Color>[
-                              Color(0xFFF2B133),
-                              Color(0xFFF2B133),
-                              Colors.orange,
-                              Colors.deepOrange,
-                            ],
-                            begin: Alignment(-1, -4),
-                            end: Alignment(1, 4),
-                          ),
-                          borderRadius: BorderRadius.circular(8),
+        appBar: AppNavigation(
+          title: 'Payment'.tr,
+          automaticallyCallBack: true,
+        ),
+        resizeToAvoidBottomInset: false,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CreditCardWidget(
+                cardNumber: cardNumber,
+                expiryDate: expiryDate,
+                cardHolderName: cardHolderName,
+                cvvCode: cvvCode,
+                bankName: 'Debit/Credit Card'.tr,
+                frontCardBorder: Border.all(color: Colors.black45),
+                backCardBorder: Border.all(color: Colors.black45),
+                showBackView: isCvvFocused,
+                obscureCardNumber: true,
+                obscureCardCvv: true,
+                isHolderNameVisible: true,
+                cardBgColor: MainColors.mainColor,
+                onCreditCardWidgetChange: (CreditCardBrand creditCardBrand) {},
+              ),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: <Widget>[
+                      CreditCardForm(
+                        formKey: formKey,
+                        obscureCvv: true,
+                        obscureNumber: true,
+                        cardNumber: cardNumber,
+                        cvvCode: cvvCode,
+                        isHolderNameVisible: true,
+                        isCardNumberVisible: true,
+                        isExpiryDateVisible: true,
+                        cardHolderName: cardHolderName,
+                        expiryDate: expiryDate,
+                        themeColor: Colors.blue,
+                        textColor: Colors.black,
+                        cardNumberDecoration: InputDecoration(
+                          labelText: 'Card Number'.tr,
+                          hintText: 'XXXX XXXX XXXX XXXX',
+                          hintStyle: const TextStyle(color: Colors.grey),
+                          labelStyle: const TextStyle(color: Colors.grey),
+                          focusedBorder: border,
+                          enabledBorder: border,
                         ),
-                        padding: const EdgeInsets.symmetric(vertical: 15),
-                        width: double.infinity,
-                        alignment: Alignment.center,
-                        child: const Text(
-                          'Validate',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 14,
+                        expiryDateDecoration: InputDecoration(
+                          hintStyle: const TextStyle(color: Colors.grey),
+                          labelStyle: const TextStyle(color: Colors.grey),
+                          focusedBorder: border,
+                          enabledBorder: border,
+                          labelText: 'Expired Date'.tr,
+                          hintText: 'XX/XX',
+                        ),
+                        cvvCodeDecoration: InputDecoration(
+                          hintStyle: const TextStyle(color: Colors.grey),
+                          labelStyle: const TextStyle(color: Colors.grey),
+                          focusedBorder: border,
+                          enabledBorder: border,
+                          labelText: 'CVV',
+                          hintText: 'XXX',
+                        ),
+                        cardHolderDecoration: InputDecoration(
+                          hintStyle: const TextStyle(color: Colors.grey),
+                          labelStyle: const TextStyle(color: Colors.grey),
+                          focusedBorder: border,
+                          enabledBorder: border,
+                          labelText: 'Card Holder'.tr,
+                        ),
+                        onCreditCardModelChange: onCreditCardModelChange,
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      GestureDetector(
+                        onTap: _onValidate,
+                        child: Container(
+                          margin: const EdgeInsets.symmetric(
+                              horizontal: 16, vertical: 8),
+                          decoration: BoxDecoration(
+                            gradient: const LinearGradient(
+                              colors: <Color>[
+                                Color(0xFFF2B133),
+                                Color(0xFFF2B133),
+                                Colors.orange,
+                                Colors.deepOrange,
+                              ],
+                              begin: Alignment(-1, -4),
+                              end: Alignment(1, 4),
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          padding: const EdgeInsets.symmetric(vertical: 15),
+                          width: double.infinity,
+                          alignment: Alignment.center,
+                          child: Text(
+                            'Validate'.tr,
+                            style: const TextStyle(
+                              color: Colors.black,
+                              fontSize: 14,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
-        bottomNavigationBar: const CustomFooterWidget()
-    );
+        bottomNavigationBar: const CustomFooterWidget());
   }
 
   void _onValidate() {
@@ -174,7 +173,8 @@ class CardInformationState extends State<CardInformation> {
             Navigator.pop(context);
             Navigator.pop(context);
             Navigator.pop(context);
-            Navigator.of(context).push(MaterialPageRoute(builder: (context) => const WalletPage()));
+            Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => const WalletPage()));
           } else {
             print('Deposit failed');
           }
@@ -187,7 +187,8 @@ class CardInformationState extends State<CardInformation> {
             Navigator.pop(context);
             Navigator.pop(context);
             Navigator.pop(context);
-            Navigator.of(context).push(MaterialPageRoute(builder: (context) => const WalletPage()));
+            Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => const WalletPage()));
           } else {
             print('Withdrawal failed');
           }
