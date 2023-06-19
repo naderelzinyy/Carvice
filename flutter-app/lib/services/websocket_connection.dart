94f7ce747fef8e8810dcf2c244f2e034cfee7ce4
Nader Elziny <naderelziny805@gmail.com>
2023-06-19 18:21:58 +0300

    Added more alerts handling for the incoming events from websocket server.

--- flutter-app/lib/services/websocket_connection.dart
+++ flutter-app/lib/services/websocket_connection.dart
@@ -4,13 +4,17 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
+import '../widgets/alerts_text_button.dart';
 import '../widgets/request_stream_widget.dart';
+import '../widgets/text_field.dart';
 
 class StreamConnection {
   final BuildContext context;
   final String role;
   final String url;
   late WebSocketChannel _channel;
+  final TextEditingController priceController = TextEditingController();
+
   Stream<dynamic>? _stream;
   StreamSubscription<dynamic>? _subscription;
 
@@ -22,49 +26,145 @@ class StreamConnection {
     _subscription = _stream?.listen(_handleData);
   }
 
+  void send(Map<String, dynamic> message) {
+    _channel.sink.add(jsonEncode(message));
+  }
+
+  void close() {
+    _subscription?.cancel();
+    _channel.sink.close();
+  }
+
+  void _showOfferDialog(dynamic receivedData) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return CustomRequestStreamAlertDialog(
+          title: 'Server message',
+          content: Text("The price is ${receivedData['price']} TL"),
+          actions: <Widget>[
+            CustomAlertButton(
+              text: 'Refuse',
+              onPressed: () {
+                send({"type": "refuse_offer", "message": "Offer refused"});
+                Navigator.of(context).pop();
+              },
+            ),
+            CustomAlertButton(
+              text: 'Accept',
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showRequestRefuseDialog(dynamic receivedData) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return CustomRequestStreamAlertDialog(
+          title: 'Request Refused',
+          content:
+              const Text("The mechanic can't accept your request right now."),
+          actions: <Widget>[
+            CustomAlertButton(
+              text: 'Ok',
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showOfferRefuseDialog(dynamic receivedData) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return CustomRequestStreamAlertDialog(
+          title: 'Offer Refused',
+          content: const Text("The client refused the offer."),
+          actions: <Widget>[
+            CustomAlertButton(
+              text: 'Ok',
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   void _handleClientSocket(dynamic receivedData) {
     if (receivedData != null) {
       var message = receivedData["message"];
       var type = receivedData["type"];
       // Show dialog or perform any desired action
       if (type == "show_offer") {
-        showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return CustomRequestStreamAlertDialog(
-              title: 'Server message',
-              content: Text(message),
-            );
-          },
-        );
+        _showOfferDialog(receivedData);
+      } else if (type == "refuse_request") {
+        _showRequestRefuseDialog(receivedData);
       }
     }
   }
 
+  void _showRequestDialog(dynamic receivedData) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return CustomRequestStreamAlertDialog(
+          title: 'Server message',
+          content: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(receivedData["client_name"] as String),
+              Text(receivedData["car_brand"] as String),
+              Text(receivedData["car_model"] as String),
+              Text(receivedData["description"] as String),
+            ],
+          ),
+          actions: <Widget>[
+            CustomTextFiled(
+                controller: priceController,
+                hintText: 'Enter price',
+                textInputType: TextInputType.number,
+                obscureText: false),
+            CustomAlertButton(
+              text: 'Refuse',
+              onPressed: () {
+                send({"type": "refuse_request", "message": "Request refused"});
+                Navigator.of(context).pop();
+              },
+            ),
+            CustomAlertButton(
+              text: 'Offer',
+              onPressed: () {
+                send({"type": "receive_offer", "price": priceController.text});
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   void _handleMechanicSocket(dynamic receivedData) {
     if (receivedData != null) {
-      var message = receivedData["message"];
       var type = receivedData["type"];
-      // Show dialog or perform any desired action
-
       if (type == "show_request") {
-        showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return CustomRequestStreamAlertDialog(
-              title: 'Server message',
-              content: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(receivedData["client_name"] as String),
-                  Text(receivedData["car_brand"] as String),
-                  Text(receivedData["description"] as String),
-                ],
-              ),
-            );
-          },
-        );
+        _showRequestDialog(receivedData);
+      } else if (type == "refuse_offer") {
+        _showOfferRefuseDialog(receivedData);
       }
     }
   }
@@ -77,14 +177,5 @@ class StreamConnection {
     } else {
       _handleMechanicSocket(receivedData);
     }
-  }
-
-  void send(Map<String, dynamic> message) {
-    _channel.sink.add(jsonEncode(message));
-  }
-
-  void close() {
-    _subscription?.cancel();
-    _channel.sink.close();
   }
 }
