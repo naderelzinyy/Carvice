a2098202be29df1e738b5c92912919f1035a264d
Nader Elziny <naderelziny805@gmail.com>
2023-06-19 07:16:03 +0300

    Added the routing of asgi application.

--- django-app/carvice_backend/asgi.py
+++ django-app/carvice_backend/asgi.py
@@ -10,10 +10,16 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from django.apps import apps as project_apps
 
+geo_request = project_apps.get_app_config("geospatial_request")
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'carvice_backend.settings')
 
 application = ProtocolTypeRouter({
-    'http': get_asgi_application()
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(
+        URLRouter(geo_request.websocket_urlpatterns)
+    )
 })
