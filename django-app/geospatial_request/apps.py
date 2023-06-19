eefb3ef4f8d1b376556d1775db057bd9916abb6d
Nader Elziny <naderelziny805@gmail.com>
2023-06-19 07:14:55 +0300

    Added websocket_urlpatterns to GeospatialRequestConfig class.

--- django-app/geospatial_request/apps.py
+++ django-app/geospatial_request/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
+from . import routing
 
 
 class GeospatialRequestConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'geospatial_request'
+    websocket_urlpatterns = routing.websocket_urlpatterns
