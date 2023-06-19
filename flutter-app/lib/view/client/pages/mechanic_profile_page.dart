f79543c117326810f661ef98f6fe6bc38560a669
Nader Elziny <naderelziny805@gmail.com>
2023-06-20 02:40:35 +0300

    Passed the mechanic id to MechanicProfile.

--- flutter-app/lib/view/client/pages/mechanic_profile_page.dart
+++ flutter-app/lib/view/client/pages/mechanic_profile_page.dart
@@ -1,27 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-
 import '../../../widgets/app_navigation.dart';
 import '../../../widgets/custom_app_footer.dart';
 import '../../../widgets/mechanic_portfolio.dart';
 
-
 class MechanicProfile extends StatelessWidget {
-  const MechanicProfile({Key? key});
-
+  const MechanicProfile({Key? key, this.id});
+  final String? id;
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppNavigation(
-        title: 'mechanic_portfolio'.tr,
-        automaticallyCallBack: true,
-      ),
-      body: SafeArea(
-        child: MechanicPortfolio(isClient: true)
-      ),
-      bottomNavigationBar: const CustomFooterWidget()
-    );
+        appBar: AppNavigation(
+          title: 'mechanic_portfolio'.tr,
+          automaticallyCallBack: true,
+        ),
+        body:
+            SafeArea(child: MechanicPortfolio(isClient: true, mechanicID: id)),
+        bottomNavigationBar: const CustomFooterWidget());
   }
 }
