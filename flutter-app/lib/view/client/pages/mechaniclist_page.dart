a46cb309a433752cb0497e457c4346a83c0cbbb1
Nader Elziny <naderelziny805@gmail.com>
2023-06-19 07:23:36 +0300

    Added name and id in mechanicList.

--- flutter-app/lib/view/client/pages/mechaniclist_page.dart
+++ flutter-app/lib/view/client/pages/mechaniclist_page.dart
@@ -2,10 +2,8 @@ import 'dart:convert';
 
 import 'package:carvice_frontend/widgets/app_navigation.dart';
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 
-import '../../../services/authentication.dart';
 import '../../../widgets/custom_app_footer.dart';
 import '../../../widgets/mechanic_list.dart';
 
@@ -19,39 +17,41 @@ class MechanicListPage extends StatefulWidget {
 
 class MechanicListPageState extends State<MechanicListPage> {
   List<MyMechanicItem> mechanicList = [];
-  late String? mechanics=  widget.mechanics;
-  void getMechanicList()  {
+  late String? mechanics = widget.mechanics;
+  void getMechanicList() {
     List<dynamic> mechanicsData = jsonDecode(mechanics!);
     setState(() {
       mechanicList = mechanicsData
           .map((item) => MyMechanicItem(
-          name: item["name"],
-          rate: 4.0,
-          imagePath: 'assets/images/person.jpeg'
-      ))
+              name: item?["commercial_name"],
+              id: item?["user_id"],
+              rate: 4.0,
+              imagePath: 'assets/images/person.jpeg'))
           .toList();
     });
   }
+
   @override
   void initState() {
     super.initState();
     getMechanicList();
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:  AppNavigation(title: 'available_mechanics'.tr, automaticallyCallBack: true,),
-      body: Column(
-        children: [
-          const SizedBox(height: 20),
-          Expanded(
-            child: CustomMechanicList(list: mechanicList),
-          ),
-        ],
-      ),
-      bottomNavigationBar:const CustomFooterWidget()
-    );
+        appBar: AppNavigation(
+          title: 'available_mechanics'.tr,
+          automaticallyCallBack: true,
+        ),
+        body: Column(
+          children: [
+            const SizedBox(height: 20),
+            Expanded(
+              child: CustomMechanicList(list: mechanicList),
+            ),
+          ],
+        ),
+        bottomNavigationBar: const CustomFooterWidget());
   }
 }
